a7cfbe92f86ad069873c4535cd0abb920a563954
leopapa <907025526@qq.com>
2022-08-17 16:24:59 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,4 +4,25 @@
 $.ajaxPrefilter(function(options) {
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
   options.url = 'http://www.liulongbin.top:3007' + options.url
+
+  // 统一为有权限的接口，设置headers请求头
+  if(options.url.indexOf(`/my/`) !== -1){
+    options.headers={
+      Authorization:localStorage.getItem(`token`) || ``
+    }
+  }
+
+  // 全局统一挂载complete回调函数
+  // 无论请求成功与否都会调用complete
+  options.complete = function(res){
+    console.log(res.responseJSON.status);
+    console.log(res.responseJSON.message);
+    // 在complete回调中可以使用res.responseJSON拿到服务器响应回来的数据
+    if(res.responseJSON.status === 1 && res.responseJSON.message ==="身份认证失败！"){
+        // 强制清空token
+        localStorage.removeItem(`token`)
+        // 强制跳转到登录页面
+        location.href = `/login.html`
+    }
+}
 })
